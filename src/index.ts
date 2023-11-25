26c07972d99dfd23493d05a804d0c568b2417ebc
Aaron Klinker <aaronklinker1@gmail.com>
2023-11-25 13:56:48 -0600

    fix: Print version on startup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import { cyan, dim } from "./utils/colors";
+import { blue, bold, cyan, dim, violet } from "./utils/colors";
 import routes from "./routes";
 import { createPortainerApi } from "./utils/portainer";
 import { startServer } from "./utils/server";
+import { version } from "../package.json";
 
 const port = Number(process.env.PORT || 3000);
 
+console.log(`${bold(cyan("Portainer Stack Webhooks"))} ${dim("v" + version)}`);
 console.log(
-  `${cyan("ℹ")} Server started ${dim("→")} ${cyan("http://localhost:" + port)}`
+  `${cyan("ℹ")} Server started ${dim("→")} ${violet(
+    "http://localhost:" + port
+  )}`
 );
 startServer({
   port,
