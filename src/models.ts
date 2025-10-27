4cfd0f1a732b5e1eb97828f8e41aea5dfd4e580b
Aaron <aaronklinker1@gmail.com>
2025-10-26 20:44:34 -0500

    chore: Upgrade `@aklinker1/zeta`

--- src/models.ts
+++ src/models.ts
@@ -1,26 +1,40 @@
 import { NoResponse } from "@aklinker1/zeta";
 import z from "zod";
 
-export const Stack = z.object({
-  id: z.int(),
-  name: z.string(),
-});
+export const Stack = z
+  .object({
+    id: z.int(),
+    name: z.string(),
+  })
+  .meta({
+    ref: "Stack",
+  });
 export type Stack = z.infer<typeof Stack>;
 
-export const ListStacksOutput = Stack.array();
+export const ListStacksOutput = Stack.array().meta({
+  ref: "ListStacksOutput",
+});
 export type ListStacksOutput = z.infer<typeof ListStacksOutput>;
 
-export const GetHealthOutput = z.object({
-  status: z.string(),
-  uptime: z.int().min(0).describe("Uptime in seconds"),
-  since: z.iso.datetime(),
-  version: z.string(),
-});
+export const GetHealthOutput = z
+  .object({
+    status: z.string(),
+    uptime: z.int().min(0).describe("Uptime in seconds"),
+    since: z.iso.datetime(),
+    version: z.string(),
+  })
+  .meta({
+    ref: "GetHealthOutput",
+  });
 export type GetHealthOutput = z.infer<typeof GetHealthOutput>;
 
-export const UpdateStackWebhookInput = z.object({
-  stackId: z.coerce.number().int().min(0),
-});
+export const UpdateStackWebhookInput = z
+  .object({
+    stackId: z.coerce.number().int().min(0),
+  })
+  .meta({
+    ref: "UpdateStackWebhookInput",
+  });
 export type UpdateStackWebhookInput = z.infer<typeof UpdateStackWebhookInput>;
 
 export const UpdateStackWebhookOutput = NoResponse.meta({
